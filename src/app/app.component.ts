5965e2fdf54bf495fd377123a85c028e50da8d5b
Donfack Pavel <noubissiepavel@gmail.com>
2023-09-28 20:35:47 +0200

    dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { Component } from '@angular/core';
-import { NgbAccordionConfig, NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginserviceService } from './service/loginservice.service';
+import {Component} from '@angular/core';
+import {LoginserviceService} from './service/loginservice.service';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +10,7 @@ export class AppComponent {
   title = 'ixtima_frontendfinal';
   	constructor( private login: LoginserviceService) {
   		//config: NgbAccordionConfig,config.closeOthers = true; customize default values of accordions used by this component tree
-  		
+
   	}
 	ngOninit(){}
   		images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
